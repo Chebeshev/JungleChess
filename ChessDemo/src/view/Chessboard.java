7226b3fccc7b05e7c4b6c7996a3f9534b8836a28
Chebeshev <132576138+Chebeshev@users.noreply.github.com>
2023-05-29 21:44:31 +0800

    Add files via upload

---
+++ ChessDemo/src/view/Chessboard.java
@@ -0,0 +1,157 @@
+package view;
+
+
+import model.*;
+import controller.ClickController;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+/**
+ * 这个类表示面板上的棋盘组件对象
+ */
+public class Chessboard extends JComponent {
+    /**
+     * CHESSBOARD_SIZE： 棋盘是8 * 8的
+     * <br>
+     * BACKGROUND_COLORS: 棋盘的两种背景颜色
+     * <br>
+     * chessListener：棋盘监听棋子的行动
+     * <br>
+     * chessboard: 表示8 * 8的棋盘
+     * <br>
+     * currentColor: 当前行棋方
+     */
+    private static final int CHESSBOARD_ROW = 9;
+    private static final int CHESSBOARD_COL = 7;
+
+    private final ChessComponent[][] chessComponents = new ChessComponent[CHESSBOARD_ROW][CHESSBOARD_COL];
+    private ChessColor currentColor = ChessColor.RED;
+    //all chessComponents in this chessboard are shared only one model controller
+    private final ClickController clickController = new ClickController(this);
+    private final int CHESS_SIZE;
+
+
+    public Chessboard(int width, int height) {
+        setLayout(null); // Use absolute layout.
+        setSize(width, height);
+        CHESS_SIZE = width / 9;
+        System.out.printf("chessboard size = %d, chess size = %d\n", width, CHESS_SIZE);
+
+        initiateEmptyChessboard();
+
+        // FIXME: Initialize chessboard for testing only.
+        initElephantOnBoard(0, 0, ChessColor.RED);
+        initElephantOnBoard(0, CHESSBOARD_COL - 1, ChessColor.RED);
+    }
+
+    public ChessComponent[][] getChessComponents() {
+        return chessComponents;
+    }
+
+    public ChessColor getCurrentColor() {
+        return currentColor;
+    }
+
+    public void putChessOnBoard(ChessComponent chessComponent) {
+        int row = chessComponent.getChessboardPoint().getX(), col = chessComponent.getChessboardPoint().getY();
+
+        if (chessComponents[row][col] != null) {
+            remove(chessComponents[row][col]);
+        }
+        add(chessComponents[row][col] = chessComponent);
+    }
+
+    public void swapChessComponents(ChessComponent chess1, ChessComponent chess2) {
+        // Note that chess1 has higher priority, 'destroys' chess2 if exists.
+        if (!(chess2 instanceof EmptySlotComponent)) {
+            remove(chess2);
+            add(chess2 = new EmptySlotComponent(chess2.getChessboardPoint(), chess2.getLocation(), clickController, CHESS_SIZE));
+        }
+        chess1.swapLocation(chess2);
+        int row1 = chess1.getChessboardPoint().getX(), col1 = chess1.getChessboardPoint().getY();
+        chessComponents[row1][col1] = chess1;
+        int row2 = chess2.getChessboardPoint().getX(), col2 = chess2.getChessboardPoint().getY();
+        chessComponents[row2][col2] = chess2;
+
+        chess1.repaint();
+        chess2.repaint();
+    }
+
+    public void initiateEmptyChessboard() {
+        for (int i = 0; i < chessComponents.length; i++) {
+            for (int j = 0; j < chessComponents[i].length; j++) {
+                putChessOnBoard(new EmptySlotComponent(new ChessboardPoint(i, j), calculatePoint(i, j), clickController, CHESS_SIZE));
+            }
+        }
+    }
+
+    public void swapColor() {
+        currentColor = currentColor == ChessColor.RED ? ChessColor.WHITE : ChessColor.RED;
+    }
+
+    private void initElephantOnBoard(int row, int col, ChessColor color) {
+        ChessComponent chessComponent = new ElephantChessComponent(new ChessboardPoint(row, col), calculatePoint(row, col), color, clickController, CHESS_SIZE);
+        chessComponent.setVisible(true);
+        putChessOnBoard(chessComponent);
+    }
+
+    private void initLionOnBoard(int row, int col, ChessColor color) {
+        ChessComponent chessComponent = new LionChessComponent(new ChessboardPoint(row, col), calculatePoint(row, col), color, clickController, CHESS_SIZE);
+        chessComponent.setVisible(true);
+        putChessOnBoard(chessComponent);
+    }
+
+    private void initTigerOnBoard(int row, int col, ChessColor color) {
+        ChessComponent chessComponent = new TigerChessComponent(new ChessboardPoint(row, col), calculatePoint(row, col), color, clickController, CHESS_SIZE);
+        chessComponent.setVisible(true);
+        putChessOnBoard(chessComponent);
+    }
+
+    private void initLeopardOnBoard(int row, int col, ChessColor color) {
+        ChessComponent chessComponent = new LeopardChessComponent(new ChessboardPoint(row, col), calculatePoint(row, col), color, clickController, CHESS_SIZE);
+        chessComponent.setVisible(true);
+        putChessOnBoard(chessComponent);
+    }
+
+    private void initWolfOnBoard(int row, int col, ChessColor color) {
+        ChessComponent chessComponent = new WolfChessComponent(new ChessboardPoint(row, col), calculatePoint(row, col), color, clickController, CHESS_SIZE);
+        chessComponent.setVisible(true);
+        putChessOnBoard(chessComponent);
+    }
+
+    private void initDogOnBoard(int row, int col, ChessColor color) {
+        ChessComponent chessComponent = new DogChessComponent(new ChessboardPoint(row, col), calculatePoint(row, col), color, clickController, CHESS_SIZE);
+        chessComponent.setVisible(true);
+        putChessOnBoard(chessComponent);
+    }
+
+    private void initCatOnBoard(int row, int col, ChessColor color) {
+        ChessComponent chessComponent = new CatChessComponent(new ChessboardPoint(row, col), calculatePoint(row, col), color, clickController, CHESS_SIZE);
+        chessComponent.setVisible(true);
+        putChessOnBoard(chessComponent);
+    }
+
+    private void initRatOnBoard(int row, int col, ChessColor color) {
+        ChessComponent chessComponent = new RatChessComponent(new ChessboardPoint(row, col), calculatePoint(row, col), color, clickController, CHESS_SIZE);
+        chessComponent.setVisible(true);
+        putChessOnBoard(chessComponent);
+    }
+
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    }
+
+
+    private Point calculatePoint(int row, int col) {
+        return new Point(col * CHESS_SIZE, row * CHESS_SIZE);
+    }
+
+    public void loadGame(List<String> chessData) {
+        chessData.forEach(System.out::println);
+    }
+}
